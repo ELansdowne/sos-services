c567e9ce2dd3d25f5f049da001ecf592a4c73871
ELansdowne <elansdowneuk@gmail.com>
2019-01-04 01:35:52 +0530

    SOS

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql');
 
  connection = mysql.createConnection({
-    host: "localhost",
+    host: "amnok.cjss6d6dlgc9.ap-south-1.rds.amazonaws.com",
     port:"3306",
     user: "root",
     password: "password",
